60059c25cf8204e5d2262eefa99bd30661be05fd
maneptha <maneptha@gmail.com>
2019-02-24 11:37:56 +0100

    selectionSort and bubbleSort

---
+++ javascript/src/4-Sorting.js
@@ -0,0 +1,42 @@
+function selectionSort(array) {
+  for (let len = array.length - 1; len > 0; len--) {
+    let maxPos = 0;
+
+    for (let i = 1; i < len + 1; i++) {
+      if (array[i] > array[maxPos]) {
+        maxPos = i;
+      }
+    }
+
+    let temp = array[len];
+    array[len] = array[maxPos];
+    array[maxPos] = temp;
+  }
+
+  return array;
+}
+
+function bubbleSort(array) {
+  for (let len = array.length - 1; len > 0; len--) {
+    let sorted = true;
+
+    for (let i = 0; i < len; i++) {
+      const a = array[i];
+      const b = array[i + 1];
+
+      if (a > b) {
+        array[i] = b;
+        array[i + 1] = a;
+        sorted = false;
+      }
+    }
+
+    if (sorted) {
+      return array;
+    }
+  }
+
+  return array;
+}
+
+module.exports = { selectionSort, bubbleSort };
---
+++ javascript/src/4-Sorting.test.js
@@ -0,0 +1,15 @@
+const { selectionSort, bubbleSort } = require("./4-Sorting");
+
+const data = [
+  [[5, 2, 8, 14, 1, 16], [1, 2, 5, 8, 14, 16]],
+  [[54, 26, 93, 17, 77, 31, 44, 55, 20], [17, 20, 26, 31, 44, 54, 55, 77, 93]],
+  [[1, 2, 3, 4, 5], [1, 2, 3, 4, 5]]
+];
+
+test("bubbleSort", () => {
+  data.forEach(([a, b]) => expect(bubbleSort([...a])).toEqual(b));
+});
+
+test("selection sort", () => {
+  data.forEach(([a, b]) => expect(selectionSort([...a])).toEqual(b));
+});
