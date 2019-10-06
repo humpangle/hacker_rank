9940302f58b121e65156f5a3bd31e49e6e96b367
humpangle <humpangle@gmail.com>
2019-10-06 13:42:23 +0200

    Setup python

--- python/linked-list-cycle/linked-list-cycle.py
+++ python/linked-list-cycle/linked-list-cycle.py
@@ -1,28 +1,39 @@
-def has_cycle0(head):
-    if head is None:
-        return False
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
 
-    def cycle(node, seen=[]):
-        d, n = node.data, node.next
-        if n is None:
-            return False
-        s = (d, n.data)
-        if s in seen:
-            return True
-        seen.append(s)
-        return cycle(n, seen)
 
-    return cycle(head, [])
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+        self.tail = node
 
 
 def has_cycle(head):
-    if head is None:
-        return False
+    slow, fast = head, head
+    while slow and fast and fast.next:
+        slow, fast = slow.next, fast.next.next
+        if slow == fast:
+            return True
+    return False
 
-    slow, fast = head, head.next
-    while slow != fast:
-        if fast is None or fast.next is None:
-            return False
-        slow = slow.next
-        fast = fast.next.next
-    return True
+
+def node_from_question(string):
+    string = string.split('\n')
+    node_count = int(string[1])
+    linked_list = SinglyLinkedList()
+
+    for node_data_index in range(node_count):
+        data = string[4 + node_data_index - 1]
+        linked_list.insert_node(int(data))
